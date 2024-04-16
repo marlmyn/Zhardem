2535d814d2ee46afa5072d741cd54f3dbc38b4d1
Akmaral <e.akmaral024@mail.ru>
2024-04-17 00:34:51 +0500

    Refactoring Code &  Add Schedule & Message VC

---
+++ Zhardem/Controller/DoctorLandscapeCollectionView/TopDoctorCollectionView.swift
@@ -0,0 +1,20 @@
+//
+//  TopDoctorCollectionView.swift
+//  Zhardem
+//
+//  Created by Akmaral Ergesh on 15.04.2024.
+//
+
+import UIKit
+
+class TopDoctorCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Zhardem/Controller/DoctorLandscapeCollectionView/TopDoctorCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TopDoctorCollectionViewCell.swift
+//  Zhardem
+//
+//  Created by Akmaral Ergesh on 15.04.2024.
+//
+
+import UIKit
+
+class TopDoctorCollectionViewCell: UICollectionViewCell {
+    
+}
